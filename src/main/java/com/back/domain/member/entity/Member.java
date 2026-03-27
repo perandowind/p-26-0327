a257cec14fde0054ef59bbb4617bc1ddc34ce999
perandowind <perandowind@gmail.com>
2026-03-27 14:06:36 +0900

    12, 13.

--- src/main/java/com/back/domain/member/entity/Member.java
+++ src/main/java/com/back/domain/member/entity/Member.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Entity
 @Getter
 @NoArgsConstructor
@@ -15,15 +17,17 @@ public class Member extends BaseEntity {
     private String username;
     private String password;
     private String nickname;
+    @Column(unique = true)
+    private String apiKey;
 
     public Member(String username, String password, String nickname) {
         this.username = username;
         this.password = password;
         this.nickname = nickname;
+        this.apiKey = UUID.randomUUID().toString();
     }
 
     public String getName() {
         return nickname;
     }
-
 }
