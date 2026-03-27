cfbc30272a9402ad5a8fab1c8b1f5ffee682bee4
perandowind <perandowind@gmail.com>
2026-03-27 15:37:12 +0900

    17 로그인 기능 구현

--- src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
@@ -3,6 +3,7 @@ package com.back.domain.member.controller;
 import com.back.domain.member.dto.MemberDto;
 import com.back.domain.member.entity.Member;
 import com.back.domain.member.service.MemberService;
+import com.back.global.exception.ServiceException;
 import com.back.global.rsData.RsData;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,13 +23,15 @@ public class ApiV1MemberController {
             String username,
             String password,
             String nickname
-    ){}
+    ) {
+    }
 
     record MemberJoinResBody(
             MemberDto memberDto
-    ){}
+    ) {
+    }
 
-    @PostMapping
+    @PostMapping("/join")
     public RsData<MemberJoinResBody> join(@RequestBody @Valid MemberJoinReqBody reqBody) {
 
         Member member = memberService.join(reqBody.username, reqBody.password(), reqBody.nickname());
@@ -41,4 +44,37 @@ public class ApiV1MemberController {
                 )
         );
     }
+
+
+    record MemberLoginReqBody(
+            String username,
+            String password
+    ) {
+    }
+
+    record MemberLoginResBody(
+            String apiKey
+    ) {
+    }
+
+    @PostMapping("/login")
+    public RsData<MemberLoginResBody> login(@RequestBody @Valid MemberLoginReqBody reqBody) {
+
+        Member actor = memberService.findByUsername(reqBody.username).orElseThrow(
+                () -> new ServiceException("401-1", "존재하지 않는 아이디입니다.")
+        );
+
+        if (!actor.getPassword().equals(reqBody.password)) {
+            throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
+        }
+
+        return new RsData<>(
+                "%s님 환영합니다.".formatted(actor.getName()),
+                "200-1",
+                new MemberLoginResBody(
+                        actor.getApiKey()
+                )
+        );
+    }
+
 }
