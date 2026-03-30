881bc3c47b18793e05f344851e82e36e3139155d
perandowind <perandowind@gmail.com>
2026-03-30 11:20:38 +0900

    24 요청데이터 처리 클래스 Rq, 스프링 빈의 싱글톤을 극복하는 @RequestScope

---
+++ src/main/java/com/back/global/rq/Rq.java
@@ -0,0 +1,10 @@
+package com.back.global.rq;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class Rq {
+
+}
