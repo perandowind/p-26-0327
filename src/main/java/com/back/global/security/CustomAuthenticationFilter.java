8b6b97c1b49a15a534feddd0d78c417cc28940ce
perandowind <perandowind@gmail.com>
2026-04-02 15:52:34 +0900

    84 SecurityContextHolder 에 인증데이터 저장

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -9,6 +9,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -93,6 +98,23 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
         }
 
         // todo: SecurityContextHolder 에 인증데이터 저장
+
+        // Spring Security 가 사용하는 회원정보
+        UserDetails user = new User(
+                member.getUsername(),
+                member.getPassword(),
+                List.of()
+        );
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                user,
+                null,
+                user.getAuthorities()
+        );
+
+        SecurityContextHolder
+                .getContext()
+                        .setAuthentication(authentication);
 
         filterChain.doFilter(request, response);
     }
